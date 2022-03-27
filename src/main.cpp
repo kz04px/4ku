4901dddc1315f097dd1da4d9ba8b952c9704f025
kz04px <6536046+kz04px@users.noreply.github.com>
2022-03-27 01:33:59 +0000

    Remove some newlines

--- src/main.cpp
+++ src/main.cpp
@@ -98,7 +98,6 @@ bool operator==(Move &lhs, Move &rhs) {
 
 void move_str(Move &move, char *str, bool flip) {
     auto promos = "\0nbrq\0\0";
-
     str[0] = (move.from % 8) + 'a';
     str[2] = (move.to % 8) + 'a';
     if (flip) {
@@ -108,7 +107,6 @@ void move_str(Move &move, char *str, bool flip) {
         str[1] = (move.from / 8) + '1';
         str[3] = (move.to / 8) + '1';
     }
-
     str[4] = promos[move.promo];
     str[5] = '\0';
 }
@@ -183,7 +181,6 @@ bool attacked(Position &pos, int sq, bool them) {
     BB RQ = pos.pieces[Rook] | pos.pieces[Queen];
     BB pawns = pos.colour[them] & pos.pieces[Pawn];
     BB pawn_attacks = them ? sw(pawns) | se(pawns) : nw(pawns) | ne(pawns);
-
     return (pawn_attacks & bb) | (kt & knight(sq, 0)) |
            (bishop(sq, pos.colour[0] | pos.colour[1]) & pos.colour[them] & BQ) |
            (rook(sq, pos.colour[0] | pos.colour[1]) & pos.colour[them] & RQ) |
@@ -193,45 +190,35 @@ bool attacked(Position &pos, int sq, bool them) {
 bool makemove(Position &pos, Move &move) {
     int piece = piece_on(pos, move.from);
     int captured = piece_on(pos, move.to);
-
     pos.colour[0] ^= (1ULL << move.from) | (1ULL << move.to);
     pos.pieces[piece] ^= (1ULL << move.from) | (1ULL << move.to);
-
     if (piece == Pawn && (1ULL << move.to) == pos.ep) {
         pos.colour[1] ^= (1ULL << (move.to - 8));
         pos.pieces[Pawn] ^= (1ULL << (move.to - 8));
     }
-
     pos.ep = 0x0ULL;
-
     if (piece == Pawn && move.to - move.from == 16) {
         pos.ep = (1ULL << (move.from + 8));
     }
-
     if (captured != None) {
         pos.colour[1] ^= (1ULL << move.to);
         pos.pieces[captured] ^= (1ULL << move.to);
     }
-
     if (piece == King) {
         BB bb = move.to - move.from == 2 ? 0xa0ULL : move.to - move.from == -2 ? 0x9ULL : 0x0ULL;
         pos.colour[0] ^= bb;
         pos.pieces[Rook] ^= bb;
     }
-
     if (piece == Pawn && move.to >= 56) {
         pos.pieces[Pawn] ^= (1ULL << move.to);
         pos.pieces[move.promo] ^= (1ULL << move.to);
     }
-
     BB changed = (1ULL << move.to) | (1ULL << move.from);
     pos.castling[0] &= !(changed & 0x90ULL);
     pos.castling[1] &= !(changed & 0x11ULL);
     pos.castling[2] &= !(changed & 0x9000000000000000ULL);
     pos.castling[3] &= !(changed & 0x1100000000000000ULL);
-
     flip(pos);
-
     int ksq = lsb(pos.colour[1] & pos.pieces[King]);
     return !attacked(pos, ksq, false);
 }
@@ -245,7 +232,6 @@ void generate_pawn_moves(Move *movelist, int &num_moves, BB to_mask, int offset)
     while (to_mask) {
         int to = lsb(to_mask);
         to_mask &= to_mask - 1;
-
         if (to >= 56) {
             add_move(movelist, num_moves, to + offset, to, Queen);
             add_move(movelist, num_moves, to + offset, to, Rook);
@@ -262,14 +248,11 @@ void generate_piece_moves(Move *movelist, int &num_moves, Position &pos, int pie
     while (copy) {
         int fr = lsb(copy);
         copy &= copy - 1;
-
         BB moves = func(fr, pos.colour[0] | pos.colour[1]);
         moves &= ~pos.colour[0];
-
         while (moves) {
             int to = lsb(moves);
             moves &= moves - 1;
-
             add_move(movelist, num_moves, fr, to, None);
         }
     }
@@ -277,11 +260,9 @@ void generate_piece_moves(Move *movelist, int &num_moves, Position &pos, int pie
 
 int movegen(Position &pos, Move *movelist) {
     int num_moves = 0;
-
     BB all = pos.colour[0] | pos.colour[1];
     BB empty = ~all;
     BB pawns = pos.colour[0] & pos.pieces[Pawn];
-
     generate_pawn_moves(movelist, num_moves, north(pawns) & empty, -8);
     generate_pawn_moves(movelist, num_moves, north(north(pawns & 0xFF00ULL) & empty) & empty, -16);
     generate_pawn_moves(movelist, num_moves, nw(pawns) & (pos.colour[1] | pos.ep), -7);
@@ -292,15 +273,12 @@ int movegen(Position &pos, Move *movelist) {
     generate_piece_moves(movelist, num_moves, pos, Rook, rook);
     generate_piece_moves(movelist, num_moves, pos, Queen, rook);
     generate_piece_moves(movelist, num_moves, pos, King, king);
-
     if (pos.castling[0] && !(all & (0x60ULL)) && !attacked(pos, 4, true) && !attacked(pos, 5, true)) {
         add_move(movelist, num_moves, 4, 6, None);
     }
-
     if (pos.castling[1] && !(all & (0xEULL)) && !attacked(pos, 4, true) && !attacked(pos, 3, true)) {
         add_move(movelist, num_moves, 4, 2, None);
     }
-
     return num_moves;
 }
 
@@ -313,28 +291,22 @@ int rook_rank78 = 24;
 
 int eval(Position &pos) {
     int score = 10;
-
     for (int c = 0; c < 2; ++c) {
         BB pawns[2];
         for (int c2 = 0; c2 < 2; ++c2) {
             pawns[c2] = pos.colour[c2] & pos.pieces[Pawn];
         }
-
         for (int p = 0; p < 6; ++p) {
             BB copy = pos.colour[0] & pos.pieces[p];
             while (copy) {
                 int sq = lsb(copy);
                 copy &= copy - 1;
-
                 int rank = sq >> 3;
                 int file = sq & 7;
-
                 int centrality = (7 - abs(7 - rank - file) - abs(rank - file)) / 2;
                 score += centrality * centralities[p];
-
                 if (p == Pawn) {
                     BB bb = 1ULL << sq;
-
                     BB attack = nw(bb) | ne(bb);
                     for (int i = 0; i < 4; ++i) {
                         attack |= north(attack);
@@ -351,36 +323,29 @@ int eval(Position &pos) {
                             score += rook_semi_open;
                         }
                     }
-
                     if (rank >= 6) {
                         score += rook_rank78;
                     }
                 }
-
                 score += material[p];
             }
         }
-
         flip(pos);
         score = -score;
     }
-
     return score;
 }
 
 int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long int stop_time, Move *pvline) {
     int ksq = lsb(pos.colour[0] & pos.pieces[King]);
     auto in_check = attacked(pos, ksq, true);
-
     depth += in_check;
-
     int static_eval = eval(pos);
     bool in_qsearch = depth <= 0;
     if (in_qsearch) {
         if (static_eval >= beta) {
             return beta;
         }
-
         if (alpha < static_eval) {
             alpha = static_eval;
         }
@@ -390,18 +355,14 @@ int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long 
             return beta;
         }
     }
-
     if (now() >= stop_time) {
         return 0;
     }
-
     Move moves[256];
     int num_moves = movegen(pos, moves);
-
     int move_scores[256];
     for (int j = 0; j < num_moves; ++j) {
         int move_score = 0;
-
         if (!in_qsearch && moves[j] == pvline[ply]) {
             move_score = 1 << 16;
         } else {
@@ -412,7 +373,6 @@ int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long 
         }
         move_scores[j] = move_score;
     }
-
     int best_score = -INF;
     for (int i = 0; i < num_moves; ++i) {
         int best_move_score = 0;
@@ -423,21 +383,16 @@ int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long 
                 best_move_score_index = j;
             }
         }
-
         auto move = moves[best_move_score_index];
         moves[best_move_score_index] = moves[i];
         move_scores[best_move_score_index] = move_scores[i];
-
         if (in_qsearch && piece_on(pos, move.to) == None) {
             break;
         }
-
         auto npos = pos;
-
         if (!makemove(npos, move)) {
             continue;
         }
-
         int new_beta = -alpha;
         int new_alpha = -alpha - 1;
         goto do_search;
@@ -448,21 +403,17 @@ int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long 
         if (score > alpha && new_alpha != -beta) {
             goto full_search;
         }
-
         if (score > best_score) {
             best_score = score;
-
             if (score > alpha) {
                 alpha = score;
                 pvline[ply] = move;
             }
         }
-
         if (alpha >= beta) {
             break;
         }
     }
-
     if (!in_qsearch && best_score == -INF) {
         if (in_check) {
             return -MATE_SCORE;
@@ -470,7 +421,6 @@ int alphabeta(Position &pos, int alpha, int beta, int depth, int ply, long long 
             return 0;
         }
     }
-
     return alpha;
 }
 
@@ -478,16 +428,11 @@ int main() {
     setbuf(stdout, NULL);
     Position pos;
     Move moves[256];
-
     getchar();
-
     puts("id name 4ku2\nid author kz04px\nuciok");
-
     while (true) {
         string word;
-
         cin >> word;
-
         if (word == "quit") {
             break;
         } else if (word == "isready") {
@@ -495,27 +440,20 @@ int main() {
         } else if (word == "go") {
             int wtime;
             int btime;
-
             cin >> word;
             cin >> wtime;
-
             cin >> word;
             cin >> btime;
-
             auto stop_time = now() + (pos.flipped ? btime : wtime) / 30;
             char bestmove_str[] = "bestmove       ";
             Move pvline[128];
-
             for (int i = 1; i < 128; ++i) {
                 alphabeta(pos, -INF, INF, i, 0, stop_time, pvline);
-
                 if (now() >= stop_time) {
                     break;
                 }
-
                 move_str(pvline[0], &bestmove_str[9], pos.flipped);
             }
-
             puts(bestmove_str);
         } else if (word == "position") {
             pos = Position();
