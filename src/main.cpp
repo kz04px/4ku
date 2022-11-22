48d979df8fd54ccd0173a1e870da0a6a742cf749
lambda0x00 <human11111010100@gmail.com>
2022-11-22 16:58:32 -0500

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -371,11 +371,11 @@ int alphabeta(Position &pos,
               const long long int stop_time,
               Stack *const stack,
               vector<Position> &history,
-              int do_null = 1) {
+              int do_null = true) {
     const int ksq = lsb(pos.colour[0] & pos.pieces[King]);
     const auto in_check = attacked(pos, ksq);
     const int static_eval = eval(pos);
-    int raised_alpha = 0;
+    int raised_alpha = false;
 
     // Check extensions
     depth += in_check;
@@ -408,7 +408,7 @@ int alphabeta(Position &pos,
             auto npos = pos;
             flip(npos);
             npos.ep = 0;
-            if (-alphabeta(npos, -beta, -beta + 1, depth - 3, ply + 1, stop_time, stack, history, 0) >= beta) {
+            if (-alphabeta(npos, -beta, -beta + 1, depth - 3, ply + 1, stop_time, stack, history, false) >= beta) {
                 return beta;
             }
         }
@@ -476,7 +476,7 @@ int alphabeta(Position &pos,
         if (score > best_score) {
             best_score = score;
             if (score > alpha) {
-            	raised_alpha = 1;
+            	raised_alpha = true;
                 alpha = score;
                 stack[ply].move = move;
             }
