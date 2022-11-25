7b13d88b34c6de9272b63bca037ca3a9294ac50c
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2022-11-25 11:13:04 +0200

    Fix TT (#14)
    Co-authored-by: Gediminas Masaitis <gmasaitis@sensata.com>

--- minifier/minify.py
+++ minifier/minify.py
@@ -339,6 +339,7 @@ def rename(tokens):
         "tt_flag":"cr",
         "TT_Entry":"cs",
         "MAX_TT_SIZE":"ct",
+        "best_move":"cu",
         # Labels
         "do_search":"bk",
         "full_search":"bl",
