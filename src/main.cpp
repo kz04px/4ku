111165a4b2511570754131c5ca71cf8ec42e41a7
Gian-Carlo Pascutto <gcp@sjeng.org>
2022-12-19 14:14:34 +0100

    Forward futility pruning. (#73)
    Don't break if the TT move fails though.

--- src/main.cpp
+++ src/main.cpp
@@ -647,6 +647,10 @@ int alphabeta(Position &pos,
         if (in_qsearch && !in_check && static_eval + 50 + max_material[piece_on(pos, move.to)] < alpha) {
             best_score = alpha;
             break;
+        } else if (!in_qsearch && !in_check && !(move == tt_move) &&
+                   static_eval + 150 * depth + max_material[piece_on(pos, move.to)] < alpha) {
+            best_score = alpha;
+            break;
         }
 
         auto npos = pos;
