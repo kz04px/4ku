de0f7f1be47b72542ae0e3cb951159d3d87670a3
Gian-Carlo Pascutto <gcp@sjeng.org>
2023-02-08 15:12:09 +0100

    Handle capturing promotions in sort/gain calulcation.
    Optimization idea from @kz04px, it's both smaller and correctly
    handles capturing promotions.
    
    -5 bytes
    
    STC vs master:
    ELO   | 10.02 +- 6.47 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=64MB
    LLR   | 2.99 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 6344 W: 1908 L: 1725 D: 2711

--- src/main.cpp
+++ src/main.cpp
@@ -636,8 +636,8 @@ int alphabeta(Position &pos,
         // then we'll use that first and delay sorting one iteration.
         if (i == !(no_move == tt_move)) {
             for (int j = 0; j < num_moves; ++j) {
-                const auto gain = moves[j].promo != None ? moves[j].promo : piece_on(pos, moves[j].to);
-                if (gain != None) {
+                const auto gain = max_material[moves[j].promo] + max_material[piece_on(pos, moves[j].to)];
+                if (gain) {
                     move_scores[j] = gain + (1LL << 54);
                 } else if (moves[j] == stack[ply].killer) {
                     move_scores[j] = 1LL << 50;
@@ -669,7 +669,7 @@ int alphabeta(Position &pos,
         move_scores[best_move_index] = move_scores[i];
 
         // Material gain
-        const auto gain = max_material[move.promo != None ? move.promo : piece_on(pos, move.to)];
+        const auto gain = max_material[move.promo] + max_material[piece_on(pos, move.to)];
 
         // Delta pruning
         if (in_qsearch && !in_check && static_eval + 50 + gain < alpha) {
