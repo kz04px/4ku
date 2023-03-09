4f9438a907593c5973c0f4dc02dfc1a30f7026a4
uwuplant <26153075+uwuplant@users.noreply.github.com>
2023-03-09 14:46:06 -0500

    Loosen RFP margins

--- src/main.cpp
+++ src/main.cpp
@@ -564,7 +564,7 @@ int alphabeta(Position &pos,
         if (!in_check && alpha == beta - 1) {
             // Reverse futility pruning
             if (depth < 7) {
-                const int margins[] = {0, 50, 100, 200, 300, 500, 800};
+                const int margins[] = {0, 70, 150, 250, 400, 600, 900};
                 if (static_eval - margins[depth - improving] >= beta) {
                     return beta;
                 }
