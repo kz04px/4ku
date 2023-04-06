a00c3d2cb36d950be8defbc68c8812ac11c56815
Adam-Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-04-06 18:50:45 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -585,7 +585,7 @@ int alphabeta(Position &pos,
                 if (-alphabeta(npos,
                                -beta,
                                -beta + 1,
-                               depth - 3 - depth / 6 - min((static_eval - beta) / 200, 3),
+                               depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3),
                                // increment ply by two to get refutation for killer tables if it fails
                                ply + 2, 
                                // minify enable filter delete
