303cadf6d0489f4c0eb5ee4bb00099aed67c2ab1
kz04px <6536046+kz04px@users.noreply.github.com>
2023-05-01 17:24:17 +0100

    Add version number to UCI id name
    +4 bytes

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ bash build-mini.sh
 
 ## 4ku-mini Size
 ```
-4,089 bytes
+4,093 bytes
 ```
 
 ---
--- src/main.cpp
+++ src/main.cpp
@@ -1085,7 +1085,7 @@ int main(
     cin >> word;
 
     // Send UCI info
-    cout << "id name 4ku\n";
+    cout << "id name 4ku 3.0\n";
     cout << "id author kz04px\n";
     // minify enable filter delete
     cout << "option name Threads type spin default " << thread_count << " min 1 max 256\n";
