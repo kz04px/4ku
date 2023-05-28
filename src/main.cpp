e06390d7e8b218e009131c2e82f5f312982d185d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-28 23:14:07 +0800

    -6 bytes
    Credit is shared with @GediminasMasaitis who helped me test the bytes reduction
    No functional change

--- src/main.cpp
+++ src/main.cpp
@@ -441,7 +441,7 @@ const int pawn_attacked[] = {S(-64, -14), S(-155, -142)};
                         // king distance to square in front of passer
                         for (int i = 0; i < 2; ++i)
                             score += pawn_passed_king_distance[i] * (rank - 1) *
-                                     max(abs(kings[i] / 8 - (rank + 1)), abs(kings[i] % 8 - file));
+                                     max(abs(kings[i] / 8 - rank - 1), abs(kings[i] % 8 - file));
                     }
                 } else {
                     // Pawn attacks
@@ -751,7 +751,7 @@ int alphabeta(Position &pos,
         }
 
         // Late move pruning based on quiet move count
-        if (!in_check && alpha == beta - 1 && num_quiets_evaluated > (3 + depth * depth) / (2 - improving))
+        if (!in_check && alpha == beta - 1 && num_quiets_evaluated > (3 + depth * depth) >> !improving)
             break;
     }
     hash_history.pop_back();
