36264678691fc8d0c75b9fc5c1c0ef67b8b6c2c0
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-06-05 21:08:46 +0300

    Multiple bench positions (#197)
    Each entry is a different phase. FENs are taken from our training dataset. The dataset was shuffled, and random positions for each phase were drawn.
    
    On an AMD 3900X cpu:
    Bench: 2272 ms 5098282 nodes 2243962 nps

--- src/main.cpp
+++ src/main.cpp
@@ -513,7 +513,7 @@ int alphabeta(Position &pos,
               int depth,
               const int ply,
               // minify enable filter delete
-              int64_t &nodes,
+              u64 &nodes,
               // minify disable filter delete
               const int64_t stop_time,
               int &stop,
@@ -813,7 +813,8 @@ auto iteratively_deepen(Position &pos,
                         vector<u64> &hash_history,
                         // minify enable filter delete
                         int thread_id,
-                        const bool is_bench,
+                        const int bench_depth,
+                        u64 &total_nodes,
                         // minify disable filter delete
                         const int64_t start_time,
                         const int allocated_time,
@@ -821,7 +822,7 @@ auto iteratively_deepen(Position &pos,
     Stack stack[128] = {};
     int64_t hh_table[2][64][64] = {};
     // minify enable filter delete
-    int64_t nodes = 0;
+    u64 nodes = 0;
     // minify disable filter delete
 
     int score = 0;
@@ -848,9 +849,11 @@ auto iteratively_deepen(Position &pos,
             break;
 
         // minify enable filter delete
-        if (thread_id == 0) {
+        // The main search thread prints with every iteration normally, or when the target depth has finished when
+        // benchmarking
+        if (thread_id == 0 &&
+            (bench_depth == 0 || i == bench_depth && newscore < score + window && newscore > score - window)) {
             const auto elapsed = now() - start_time;
-
             cout << "info";
             cout << " depth " << i;
             cout << " score cp " << newscore;
@@ -870,15 +873,12 @@ auto iteratively_deepen(Position &pos,
                 print_pv(pos, stack[0].move, hash_history);
             }
             cout << "\n";
+        }
 
-            // OpenBench compliance
-            if (is_bench && i >= 16) {
-                cout << "Bench: ";
-                cout << elapsed << " ms ";
-                cout << nodes << " nodes ";
-                cout << nodes * 1000 / max(elapsed, static_cast<int64_t>(1)) << " nps\n";
-                break;
-            }
+        // OpenBench compliance
+        if (bench_depth > 0 && i >= bench_depth && newscore < score + window && newscore > score - window) {
+            total_nodes += nodes;
+            break;
         }
         // minify disable filter delete
 
@@ -1003,12 +1003,51 @@ int main(
 
     // minify enable filter delete
     // OpenBench compliance
+    u64 total_nodes = 0;
     if (argc > 1 && argv[1] == string("bench")) {
         // Initialise the TT
         transposition_table.resize(num_tt_entries);
 
-        int stop = false;
-        iteratively_deepen(pos, hash_history, 0, true, now(), 1 << 30, stop);
+        const pair<string, int> bench_positions[] = {
+            {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 14},         // Phase 24
+            {"r1n2rk1/1pq2pp1/4bn1p/N1bpp3/4P3/3QBP2/PPP1B1PP/2KR3R w - - 0 1", 13},  // Phase 23
+            {"r1bq1rk1/pp2p1bp/2p3p1/1P3p2/2QPn3/6P1/PB1NPPBP/R4RK1 b - - 0 1", 14},  // Phase 22
+            {"r3r1k1/ppqn1p2/4bppp/8/2PB4/1P1B2b1/P1Q2PPP/R3R1K1 w - - 0 1", 14},     // Phase 21
+            {"r4rk1/pp3ppp/2nppq2/8/2BPb3/4P3/PP1N1PPP/R2Q1RK1 b - - 0 1", 13},       // Phase 20
+            {"r2q2k1/1p1r4/p4b2/2Bn2p1/4Q3/6PP/PP3P2/R2R2K1 w - - 0 1", 12},          // Phase 19
+            {"2bq1rk1/1pn2ppp/3p4/p2Np3/4P1P1/PN2BP2/1PP3QP/2K1b2R w - - 0 1", 15},   // Phase 18
+            {"2rr3k/1p3pp1/1Q2b2p/pP2p3/P1B1P3/3P2R1/5P1P/2q3NK w - - 0 1", 15},      // Phase 17
+            {"4k2r/3nbp1p/pqp1p3/3p2p1/3P1B2/4PPN1/PP2QP1P/2R3K1 b w k - 0 1", 14},   // Phase 16
+            {"6k1/5p2/p3p1p1/2qnQ3/6P1/1r5P/1P2R2K/5R2 w - - 0 1", 13},               // Phase 15
+            {"q1b2rk1/2R1Qp1p/p5p1/1p6/1P1Np2P/P3P1P1/5P2/6K1 b - - 0 1", 15},        // Phase 14
+            {"6k1/3q2b1/1nNp4/3Pp1pp/4Pp2/5P2/6PP/2RQ2K1 w - - 0 1", 14},             // Phase 13
+            {"1r3rk1/p4p2/2p1bp1p/3Pb3/8/1P1BN3/P4PPP/4RRK1 b - - 0 1", 15},          // Phase 12
+            {"5b2/5k1p/4bpp1/2p5/4Pq2/3PN2P/2P2PP1/Q5K1 w - - 0 1", 13},              // Phase 11
+            {"3k4/2p5/p2qn3/1p1pNQ2/1P6/P6P/6P1/5K2 b - - 0 1", 14},                  // Phase 10
+            {"r1b3k1/2p2ppp/1b6/p3N3/5B2/2P5/PP4PP/R2n1K2 b - - 0 1", 14},            // Phase 9
+            {"2k5/1pp2Q1p/8/8/5P2/3q4/P5K1/8 b - - 0 1", 17},                         // Phase 8
+            {"8/1R4b1/3k4/3npp1p/3p3P/r2P1NP1/4PP2/6K1 w - - 0 1", 16},               // Phase 7
+            {"8/2p2p2/2Bb1kp1/R6p/P6P/r5P1/4PPK1/8 b - - 0 1", 13},                   // Phase 6
+            {"8/5b2/pr4p1/4k2p/8/P1K3P1/7P/5R2 w - - 0 1", 15},                       // Phase 5
+            {"8/7R/8/4p1k1/1r6/3K4/8/8 b - - 0 1", 15},                               // Phase 4
+            {"8/8/5kp1/5p1p/4pP1P/6P1/4K3/2R3b1 b - - 0 1", 22},                      // Phase 3
+            {"8/8/3bB3/7p/8/pK4kP/8/8 w - - 0 1", 25},                                // Phase 2
+            {"8/5k2/P6P/5b2/P7/3p4/3K4/8 b - - 0 1", 18},                             // Phase 1
+            {"8/8/p1Pk2pp/3p1p2/p4P2/6P1/5K1P/8 w - - 0 1", 16}                       // Phase 0
+        };
+
+        const auto start_time = now();
+        for (const auto &[fen, depth] : bench_positions) {
+            int stop = false;
+            set_fen(pos, fen);
+            iteratively_deepen(pos, hash_history, 0, depth, total_nodes, now(), 1 << 30, stop);
+        }
+        const auto elapsed = now() - start_time;
+
+        cout << "Bench: ";
+        cout << elapsed << " ms ";
+        cout << total_nodes << " nodes ";
+        cout << total_nodes * 1000 / max(elapsed, static_cast<int64_t>(1)) << " nps\n";
 
         return 0;
     }
@@ -1098,7 +1137,8 @@ int main(
                                        hash_history,
                                        // minify enable filter delete
                                        i,
-                                       false,
+                                       0,
+                                       total_nodes,
                                        // minify disable filter delete
                                        start,
                                        1 << 30,
@@ -1108,7 +1148,8 @@ int main(
                                                       hash_history,
                                                       // minify enable filter delete
                                                       0,
-                                                      false,
+                                                      0,
+                                                      total_nodes,
                                                       // minify disable filter delete
                                                       start,
                                                       allocated_time,
