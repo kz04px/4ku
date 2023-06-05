a029b7836f076b49825af97f2830d5cf89227bb9
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-06-05 20:46:05 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -849,7 +849,7 @@ auto iteratively_deepen(Position &pos,
             break;
 
         // minify enable filter delete
-        if (thread_id == 0 && bench_depth == 0) {
+        if (thread_id == 0 && (bench_depth == 0 || i == bench_depth)) {
             const auto elapsed = now() - start_time;
             cout << "info";
             cout << " depth " << i;
