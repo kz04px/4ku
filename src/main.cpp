2cbf4e27aef09555d7f9025b48b82225b676117f
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-15 16:38:07 +0800

    -1 byte

--- src/main.cpp
+++ src/main.cpp
@@ -824,7 +824,7 @@ Move iteratively_deepen(Position &pos,
 
     i32 score = 0;
     for (i32 i = 1; i < 128; ++i) {
-        i32 window = 32 + score * score / 16384;
+        i32 window = 32 + (score * score >> 14);
         i32 research = 0;
     research:
         const i32 newscore = alphabeta(pos,
