2e2d24b03d8e39a06821b1c5e784f74174132825
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-07-03 16:56:15 +0300

    Update size

--- src/main.cpp
+++ src/main.cpp
@@ -677,8 +677,8 @@ i32 alphabeta(Position &pos,
             // Late move reduction
             i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
                                 ? num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) + !improving +
-                                  (hh_table[pos.flipped][move.from][move.to] < 0) -
-                                  (hh_table[pos.flipped][move.from][move.to] > 0)
+                                      (hh_table[pos.flipped][move.from][move.to] < 0) -
+                                      (hh_table[pos.flipped][move.from][move.to] > 0)
                                 : 0;
 
         zero_window:
