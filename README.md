f16285761035028c462510eb1fcd1a452a851427
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-05-03 13:25:22 +0800

    Bench 3844068

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ bash build-mini.sh
 
 ## 4ku-mini Size
 ```
-3,942 bytes
+3,941 bytes
 ```
 
 ---
--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ enum
 [[nodiscard]] u64 now() {
     timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
-    return t.tv_sec * 1000 + t.tv_nsec / 1000000;
+    return t.tv_sec * 1000 + t.tv_nsec / 1e6;
 }
 
 struct [[nodiscard]] Position {
